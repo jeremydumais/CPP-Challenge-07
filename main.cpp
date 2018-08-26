47c9dff89aaa8fa66680e73dc164804ac46a9414
jeremydumais@hotmail.com <gIyqyc8PJNDVn0chDDqg>
2018-08-26 07:39:37 -0400

    First test with 2 loops.Way too long

--- main.cpp
+++ main.cpp
@@ -10,8 +10,26 @@ int main(int argc, char const *argv[])
     cout << "Amicable numbers smaller than 1 million" << endl;
   
     //Calculation
+    unsigned int amicableNumberCount {0};
+    for(unsigned int i=1; i<limit; i++)
+    {
+        auto firstDivisors = abundantHelper::getDivisors(i);
+        auto firstAbundance = abundantHelper::getAbundance(firstDivisors);
+        for(unsigned int j=i+1; j<limit; j++)
+        {
+            auto secondDivisors = abundantHelper::getDivisors(j);
+            auto secondAbundance = abundantHelper::getAbundance(secondDivisors);
 
-   
+            if (firstAbundance == j && secondAbundance == i)
+            {
+                if (amicableNumberCount > 0)
+                    cout << ", ";
+                cout << "(" << i <<  ", " << j << ")";
+                amicableNumberCount++;
+            }   
+        }
+    }
+
     cout << endl;
     cout << "Press any key to continue... " << endl;
     getch();
